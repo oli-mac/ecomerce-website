65b2a10bd842bc5015502f4de8f82ab0e5c4e722
oli-mac <adumulugeta21@gmail.com>
2022-12-09 12:25:02 +0300

    create next app

---
+++ ecommerce/components/Layout.js
@@ -0,0 +1,22 @@
+import { createTheme } from "@mui/material/styles";
+
+export default function Layout(title, description, Children ){
+
+    const theme = createTheme({
+        typography: {
+            h1: {
+                fontSize: '1.6rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            },
+            h2: {
+                fontSize: '1.4rem',
+                fontWeight: 400,
+                margin: '1rem 0',
+            },
+        },
+        palette: {
+            
+        }
+    });
+}
